529ee6e24abc73c5fddc891b4492a15800ae3d4a
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-10-26 14:03:41 -0300

    More Triggers

--- Assets/scripts/OrbTrigger.cs
+++ Assets/scripts/OrbTrigger.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class OrbTrigger : MonoBehaviour
 {
     public GameObject orb;
+    public GameObject orb2;
+    public GameObject orb3;
+    public GameObject orb4;
+    public GameObject Light1;
     public GameObject player;
 
+
+    private void Start()
+    {
+        Light1.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("trigger"))
@@ -16,7 +25,16 @@ public class OrbTrigger : MonoBehaviour
             
         }
 
-        
+        if (other.CompareTag("trigger2"))
+        {
+            orb2.SetActive(false);
+            orb3.SetActive(false);
+            Light1.SetActive(true);
+            Debug.Log("is this working?");
+
+        }
+       
+
     }
     
 }
